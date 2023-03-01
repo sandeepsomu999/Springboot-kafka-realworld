75eeba1e6b80b9c54323f05ac2da27896bfd2a5a
Sandeep Somu <sandeepsomu999@gmail.com>
2023-03-01 16:52:56 -0700

    Added consumer database module

--- kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/WikimediaChangesProducer.java
@@ -1,14 +1,18 @@
 package net.javaguides.springboot;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.logging.Logger;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WikimediaChangesProducer {
-    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(WikimediaChangesProducer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
 
     private KafkaTemplate<String, String> kafkaTemplate;
 
@@ -16,10 +20,21 @@ public class WikimediaChangesProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(){
+    public void sendMessage() throws InterruptedException {
         String topic = "wikimedia_recentchange";
 
+
         // to read realtime data from wikimedia we use event source
+        EventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
+
+
+
 
     }
 
